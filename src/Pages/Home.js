25a1f3ae37cda75499105da343c0713bee0398b3
rokib97 <hasanrokib1997@gmail.com>
2022-06-03 07:32:24 +0600

    initial setup react app

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6">
+          <h2>Image Will be here</h2>
+        </div>
+        <div className="col-lg-6">
+          <h2>Right sigth</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
