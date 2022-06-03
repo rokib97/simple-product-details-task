0c7eb6ee0ff23cbe108b136b7f57972caed1f90e
rokib97 <hasanrokib1997@gmail.com>
2022-06-04 04:08:51 +0600

    added fetch instead axios beacuse it was failing to fecth data

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { createContext, useEffect, useReducer } from "react";
 export const ProductContext = createContext();
 const initialState = {
@@ -31,20 +32,22 @@ const Context = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    fetch(
-      `https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "SUCCESS", payload: data });
+    // fetch(
+    //   `https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     dispatch({ type: "SUCCESS", payload: data });
+    //   })
+    //   .catch((e) => console.error(e));
+
+    axios
+      .get(
+        "https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product"
+      )
+      .then((res) => {
+        dispatch({ type: "SUCCESS", payload: res.data });
       });
-    // axios
-    //   .get(
-    //     "https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product"
-    //   )
-    //   .then((res) => {
-    //     dispatch({ type: "SUCCESS", payload: res.data });
-    //   });
   }, []);
 
   return (
